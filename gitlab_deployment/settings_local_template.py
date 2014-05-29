5873cc610b6cb2467e421600ca96d20a1710dfe2
Felix Kosmalla <felixkosmalla@gmail.com>
2014-05-29 21:57:52 +0200

    added dummy data in local settings template

--- gitlab_deployment/settings_local_template.py
+++ gitlab_deployment/settings_local_template.py
@@ -1,7 +1,16 @@
 from settings import SITE_ROOT
 import os
 
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
+
+
+STATIC_ROOT = "/home/gitploy/document_root/static/"
+
+REPOSITORY_ROOT = "/home/gitploy/repositories"
+
+GITLAB_URL = "http://git.your-company.org"
+
+DEPLOY_KEY = "ssh-rsa ......"
 
